159cf23c4aa9cbd08e9293123f667dccad5836cb
Neeraj Menon <neerajmenons@gmail.com>
2018-05-17 14:43:24 +0530

    Add files via upload

--- people/smile_detect.py
+++ people/smile_detect.py
@@ -3,7 +3,7 @@ import cv2
 #importing the cascade from the xml file
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 #detection functions
 def detect(gray, frame):
     #x and y coordinates of the left corner
@@ -18,15 +18,20 @@ def detect(gray, frame):
         cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
         roi_gray = gray[ y:y+h, x:x+w ]
         roi_color = frame[ y:y+h, x:x+w ]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
         for(ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0, 255, 0), 2)
+
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        for(sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
     return frame
     #as roi is a part of frame, drawing in roi will affect the frame
 video_capture = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video_capture.read()
+    frame = cv2.flip(frame,1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #calling the function to draw rectangles
     canvas = detect(gray, frame)
